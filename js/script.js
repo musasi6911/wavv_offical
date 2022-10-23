19a41b6cf34934a6f9c053fb9f5a157aa1eb49ff
Musasi <musasi.art@gmail.com>
2022-10-23 14:40:44 +0800

    home page

---
+++ js/script.js
@@ -0,0 +1,41 @@
+$(function(){
+
+  // Show Player
+  $('.list-square').click(function(){
+    $('.player').animate({bottom: 0}, 500);
+    return false;
+  })
+
+  
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
