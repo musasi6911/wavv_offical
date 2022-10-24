e5228dfd5dd38a9f8badc236a90bcc751d138ea1
Musasi <musasi.art@gmail.com>
2022-10-24 16:00:00 +0800

    adj details page

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 $(function(){
 
   // Show Player
-  $('.list-square a, .song-detail_more a').click(function(){
+  $('.list-square a, .song-detail_more a, .song-detail_play').click(function(){
     $('.player').animate({bottom: 0}, 500);
     return false;
   })
