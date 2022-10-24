56d525d41dba74a7576eb2f703b8515f6bc73b99
Musasi <musasi.art@gmail.com>
2022-10-24 15:52:34 +0800

    song details page

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 $(function(){
 
   // Show Player
-  $('.list-square').click(function(){
+  $('.list-square a, .song-detail_more a').click(function(){
     $('.player').animate({bottom: 0}, 500);
     return false;
   })
