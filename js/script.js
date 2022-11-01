185e8629d07892b184894621db4d689c7ede2cd7
Musasi <musasi.art@gmail.com>
2022-11-01 12:33:20 +0800

    composer page

--- js/script.js
+++ js/script.js
@@ -5,6 +5,27 @@ $(function(){
     $('.player').animate({bottom: 0}, 500);
     return false;
   })
+
+  // Composer Page
+  $('#file-upload').change(function() {
+    var i = $(this).prev('label').clone();
+    var file = $('#file-upload')[0].files[0].name;
+    $(this).next('p').text(file);
+    $(this).next('p').css('color', '#333');
+  });
+
+  var imgInp = document.getElementById('img-upload-input');
+  var blah = document.getElementById('show-img');
+
+  imgInp.onchange = evt => {
+    const [file] = imgInp.files
+    if (file) {
+      $('.img-upload p').css('display', 'none');
+      $('.img-upload i').css('display', 'none');
+      blah.style.display = 'block';
+      blah.src = URL.createObjectURL(file);
+    }
+  }
   
 
 })
